7e14fb694a2fa8349b926f12f3732de2eb7e297f
pritk <pradovic@itekako.com>
2023-07-23 01:03:53 +0000

    saving and loading of filterlists

--- src/filterlist.jl
+++ src/filterlist.jl
@@ -1,5 +1,7 @@
 module Filterlist
 
+using Serialization: serialize, deserialize
+
 using ..Utils: ThreadSafe
 import ..Nostr
 
@@ -10,5 +12,24 @@ analytics_pubkey_blocked = Set{Nostr.PubKeyId}() |> ThreadSafe
 
 access_event_blocked    = Set{Nostr.EventId}() |> ThreadSafe
 analytics_event_blocked = Set{Nostr.EventId}() |> ThreadSafe
+
+function save(dest::Union{String, IOBuffer})
+    d = Dict()
+    for n in names(@__MODULE__; all=true)
+        if !startswith(string(n), '#') && endswith(string(n), "_blocked") && (occursin("_pubkey_", string(n)) || occursin("_event_", string(n)))
+            d[n] = lock(getproperty(@__MODULE__, n)) do v; copy(v); end
+        end
+    end
+    serialize(dest, d)
+end
+
+function load(src::Union{String, IOBuffer})
+    d = deserialize(src)
+    for (n, v) in d
+        lock(getproperty(@__MODULE__, n)) do s
+            copy!(s, v)
+        end
+    end
+end
 
 end
