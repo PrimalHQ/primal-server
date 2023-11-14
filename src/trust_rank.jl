a2bfe737f59791cde0a712bfc13cd3bd8ca74164
pritk <pradovic@itekako.com>
2023-11-14 11:59:56 +0000

    small correction for trustrank based humaness check

--- src/trust_rank.jl
+++ src/trust_rank.jl
@@ -10,8 +10,8 @@ external_resources_threshold = Ref(0.0)
 function load(tr)
     merge!(pubkey_rank, tr)
     sorted = sort(collect(pubkey_rank); by=x->-x[2])
-    humaness_threshold[] = sorted[50000][2]
-    external_resources_threshold[] = sorted[50000][2]
+    humaness_threshold[] = first(sorted, 50000)[end][2]
+    external_resources_threshold[] = first(sorted, 100000)[end][2]
     nothing
 end
 
