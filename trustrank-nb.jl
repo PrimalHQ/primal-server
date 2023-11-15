adda9fa4722e8a5915300f9501b625561a609475
pritk <pradovic@itekako.com>
2023-11-15 21:10:07 +0000

    speed-up calculation of trustrank

--- trustrank-nb.jl
+++ trustrank-nb.jl
@@ -20,12 +20,20 @@ function check_nip05(pubkey, domain, name)
     end
 end
 ##
-running = Utils.PressEnterToStop()
-domains = Accumulator{String, Int}()
-@time for (i, eid) in enumerate(mdeids)
-    yield(); running[] || break
-    running[] || break
-    i % 1000 == 0 && print("$i/$(length(mdeids))    \r")
+function iterate_mdeids(body)
+    running = Utils.PressEnterToStop()
+    i = Ref(0) |> Utils.ThreadSafe
+    Threads.@threads for eid in mdeids
+        yield(); running[] || break
+        DB.incr(i)
+        i[] % 10000 == 0 && print("$(i[])/$(length(mdeids))    \r")
+        body(eid)
+    end
+    println()
+end
+##
+domains = Accumulator{String, Int}() |> Utils.ThreadSafe
+@time iterate_mdeids() do eid
     try
         md = cache_storage.events[eid]
         d = JSON.parse(md.content)
@@ -37,8 +45,7 @@ domains = Accumulator{String, Int}()
         end
     catch _ end
 end
-println()
-#
+##
 domains = sort(collect(domains); by=r->-r[2])
 open("nip05-domains-1.txt", "w+") do f
     for (domain, cnt) in domains
@@ -101,11 +108,8 @@ function parse_nip05(nip05)
     name, domain
 end
 ##
-running = Utils.PressEnterToStop()
-good_pks_unverified = Set()
-@time for (i, eid) in enumerate(mdeids)
-    yield(); running[] || break
-    i % 1000 == 0 && print("$i/$(length(mdeids))    \r")
+good_pks_unverified = Set() |> Utils.ThreadSafe
+@time iterate_mdeids() do eid
     try
         md = cache_storage.events[eid]
         d = JSON.parse(md.content)
@@ -115,15 +119,19 @@ good_pks_unverified = Set()
         end
     catch _ end
 end
+good_pks_unverified = collect(good_pks_unverified)
 ##
 good_pks_verified = Dict()
-##
+#
 running = Utils.PressEnterToStop()
 good_pks_domains = Accumulator{String, Int}()
 good_cnt = Ref(0)
-for (i, pk) in enumerate(collect(good_pks_unverified)[1:end])
-    yield(); running[] || break
-    print("$i/$(length(good_pks_unverified))/$(length(good_pks_verified))    \r")
+i = Ref(0)
+#for (i, pk) in enumerate(collect(good_pks_unverified)[1:end])
+asyncmap(collect(good_pks_unverified); ntasks=100) do pk
+    yield(); running[] || return
+    DB.incr(i)
+    print("$(i[])/$(length(good_pks_unverified))/$(length(good_pks_verified))    \r")
     md = cache_storage.events[cache_storage.meta_data[pk]]
     local d = JSON.parse(md.content)
     name, domain = parse_nip05(d["nip05"])
@@ -144,17 +152,14 @@ end
 println()
 @show length(good_pks_verified) length(good_pks_unverified) good_cnt[]
 ##
-running = Utils.PressEnterToStop()
-users = Dict()
-@time for (i, eid) in enumerate(mdeids)
-    yield(); running[] || break
-    i % 1000 == 0 && print("$i/$(length(mdeids))/$(length(users))    \r")
+users = Dict() |> Utils.ThreadSafe
+@time iterate_mdeids() do eid
     try
         md = cache_storage.events[eid]
         local d = JSON.parse(md.content)
         if haskey(d, "nip05")
             name, domain = parse_nip05(d["nip05"])
-            domain == "nostr-vip.top" && continue
+            domain == "nostr-vip.top" && return
         end
         follows = Set()
         if md.pubkey in cache_storage.contact_lists
@@ -162,17 +167,21 @@ users = Dict()
                 if length(tag.fields) >= 2 && tag.fields[1] == "p"
                     if !isnothing(local pk = try Nostr.PubKeyId(tag.fields[2]) catch _ end)
                         push!(follows, pk)
-                        get!(users, pk) do; Set{Nostr.PubKeyId}(); end
+                        lock(users) do users
+                            get!(users, pk) do; Set{Nostr.PubKeyId}(); end
+                        end
                     end
                 end
             end
         end
         users[md.pubkey] = follows
     catch ex
-        # println(ex)
+        #println(ex)
+        # rethrow()
     end
 end
 println()
+users = users.wrapped
 for (pk, follows) in users; delete!(follows, pk); end
 println("number of users: ", length(users))
 println("avg number of follows: ", sum([length(follows) for (_, follows) in users])/length(users))
@@ -212,7 +221,7 @@ end
 tr_sorted = sort(collect(zip(keys(users), tr)); by=t->-t[2])
 ##
 serialize("../tr_sorted.jls", tr_sorted)
-##
+####
 user_profiles = Dict()
 ##
 running = Utils.PressEnterToStop()
@@ -264,4 +273,21 @@ tr_sorted = deserialize("../tr_sorted.jls", )
 for (pk, t) in tr_sorted
     TrustRank.pubkey_rank[pk] = t
 end
+##
+lud16_domains = Accumulator{String, Int}() |> Utils.ThreadSafe
+th = tr_sorted[100000][2]
+@time iterate_mdeids() do eid
+    try
+        md = cache_storage.events[eid]
+        TrustRank.pubkey_rank[md.pubkey] >= th || return
+        d = JSON.parse(md.content)
+        if haskey(d, "lud16")
+            parts = split(d["lud16"], '@')
+            name = strip(parts[1])
+            domain = strip(parts[2])
+            push!(lud16_domains, domain)
+        end
+    catch _ end
+end
+lud16_domains = sort(collect(lud16_domains); by=r->-r[2])
 ##
