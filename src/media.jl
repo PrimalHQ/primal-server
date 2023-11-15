f68713d198724f46b1ce7aeafbdf0436560a87bb
pritk <pradovic@itekako.com>
2023-11-15 22:16:19 +0000

    execute conda once to install all deps for link preview generaton

--- src/media.jl
+++ src/media.jl
@@ -423,9 +423,7 @@ function fetch_resource_metadata_(url; proxy=MEDIA_PROXY[])
 end
 
 import Conda
-Conda.add("beautifulsoup4")
-Conda.add("requests")
-Conda.add("html5lib")
+Conda.add(["beautifulsoup4", "requests", "html5lib"])
 function fetch_resource_metadata(url; proxy=MEDIA_PROXY[]) 
     proxy = isnothing(proxy) ? "null" : proxy
     r = try
