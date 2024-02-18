cf91637cc47390ea4b23c18a637f9f1d20e083e9
pritk <pradovic@itekako.com>
2024-02-18 19:19:51 +0000

    additional settings for pushgateway exporter

--- src/pushgateway_exporter.jl
+++ src/pushgateway_exporter.jl
@@ -4,11 +4,14 @@ import HTTP
 
 ENABLED = Ref(false)
 JOB = Ref("default")
+HOSTPORT = Ref(("127.0.0.1", 9091))
+TIMEOUT = Ref(10)
 
 function set!(k, v; job=JOB[], type=:counter) # type is usually :counter or :gauge
     ENABLED[] || return
     s = "# TYPE $k $type\n$k $v\n"
-    HTTP.post("http://127.0.0.1:9091/metrics/job/$job", [], s)
+    HTTP.post("http://$(HOSTPORT[][1]):$(HOSTPORT[][2])/metrics/job/$job", [], s;
+              retry=false, connect_timeout=TIMEOUT[], readtimeout=TIMEOUT[])
     nothing
 end
 
