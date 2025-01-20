987b6c607e84f12b98f221d324ac5c8f8e9bfe2a
pritk <pradovic@itekako.com>
2025-01-20 21:11:01 +0000

    blossom server corrections

--- src/blossom.jl
+++ src/blossom.jl
@@ -6,6 +6,7 @@ using DataStructures: CircularBuffer
 import ..Utils
 import ..Nostr
 import ..Postgres
+import ..Media
 
 PORT = Ref(21000)
 DB = Ref(:membership)
@@ -24,13 +25,29 @@ est = Ref{Any}(nothing)
 pex(query::String, params=[]) = Postgres.execute(DB[], replace(query, '?'=>'$'), params)
 pex(server::Symbol, query::String, params=[]) = Postgres.execute(server, replace(query, '?'=>'$'), params)
 
+struct BlossomException <: Exception
+    status::Int
+    message::String
+end
+
+function blossom_error(status::Int, message::String)
+    throw(BlossomException(status, message))
+end
+
+exceptions_lock = ReentrantLock()
 function catch_exception(body::Function, handler::Symbol, args...)
     try
         body()
     catch ex
-        push!(exceptions, (handler, time(), ex, args...))
-        PRINT_EXCEPTIONS[] && Utils.print_exceptions()
-        HTTP.Response(500, "error")
+        lock(exceptions_lock) do
+            push!(exceptions, (handler, time(), ex, args...))
+            PRINT_EXCEPTIONS[] && Utils.print_exceptions()
+        end
+        if ex isa BlossomException
+            HTTP.Response(ex.status, response_headers("text/plain"; extra=["X-Reason"=>ex.message]), ex.message)
+        else
+            HTTP.Response(500, response_headers("text/plain"; extra=["X-Reason"=>"unspecified error"]), "error")
+        end
     end
 end
 
@@ -65,12 +82,21 @@ function mimetype_for_ext(ext::String)
 end
 
 function response_headers(content_type="application/json"; extra=[])
-    HTTP.Headers(["Content-Type"=>content_type,
-                  "Access-Control-Allow-Origin"=>"*",
-                  "Access-Control-Allow-Methods"=>"*",
-                  "Access-Control-Allow-Headers"=>"*",
-                  extra...
-                 ])
+    HTTP.Headers(collect(Dict(["Content-Type"=>content_type,
+                               "Access-Control-Allow-Origin"=>"*",
+                               "Access-Control-Allow-Methods"=>"*",
+                               "Access-Control-Allow-Headers"=>"*",
+                               extra...
+                              ])))
+end
+
+function get_header(headers, header)
+    for (k, v) in collect(headers)
+        if lowercase(k) == lowercase(header)
+            return v
+        end
+    end
+    nothing
 end
 
 function find_blob(req_target)
@@ -90,7 +116,7 @@ end
 function find_upload(req_target)
     h, ext = splitext(req_target[2:end])
     h = lowercase(h)
-    sha256 = hex2bytes(h)
+    sha256 = try hex2bytes(h) catch _ return nothing end
     for (mimetype, path, pubkey, key) in pex("select mimetype, path, pubkey, key::varchar from media_uploads where sha256 = ?1 limit 1", [sha256])[2]
         for (media_url, mimetype, storage_provider) in pex(:p0, "select media_url, content_type, storage_provider from media_storage where key::jsonb->>'sha256' = ?1 limit 1", [h])[2]
             storage_provider = Symbol(storage_provider)
@@ -108,42 +134,50 @@ function find_upload(req_target)
     nothing
 end
 
-function check_action(req, action::String)
+function check_action(req, action::String; x_tag_hash=nothing)
     parts = []
-    for (k, v) in collect(req.headers)
-        if lowercase(k) == "authorization"
-            parts = split(v)
-        end
+    if !isnothing(local v = get_header(req.headers, "Authorization"))
+        parts = split(v)
     end
-    @assert parts[1] == "Nostr"
+    length(parts) == 2 || blossom_error(400, "missing auth event")
+    parts[1] == "Nostr" || blossom_error(400, "invalid auth event")
     e = Nostr.Event(JSON.parse(String(Base64.base64decode(parts[2]))))
-    Nostr.verify(e) || return HTTP.Response(401, response_headers("text/plain"), "unauthorized")
-    @assert e.kind == 24242
+    Nostr.verify(e) || blossom_error(400, "auth event verification failed")
+    e.kind == 24242 || blossom_error(400, "wrong kind in auth event")
     # @assert e.created_at <= trunc(Int, time())
+    action_ok = false
+    x_tag_ok = isnothing(x_tag_hash)
     for t in e.tags
         if length(t.fields) >= 2
             if t.fields[1] == "expiration"
                 expiration = parse(Int, t.fields[2])
-                @assert expiration > trunc(Int, time())
+                expiration > trunc(Int, time()) || blossom_error(400, "auth event expired")
             elseif t.fields[1] == "t"
-                @assert action == t.fields[2]
+                action_ok |= action == t.fields[2] 
+            elseif t.fields[1] == "x" && !isnothing(x_tag_hash)
+                x_tag_ok |= hex2bytes(t.fields[2]) == x_tag_hash
             end
         end
     end
+    action_ok || blossom_error(400, "invalid action in auth event")
+    x_tag_ok || blossom_error(400, "invalid x tag")
     e
 end
 
 function blossom_handler(req::HTTP.Request)
     catch_exception(:blossom_handler, req) do
-        host = Dict(req.headers)["Host"]
+        host = get_header(req.headers, "Host")
         # @show (req.method, req.target)
         
         if req.method == "OPTIONS"
-            return HTTP.Response(200, response_headers("text/plain"), "ok")
+            return HTTP.Response(200, response_headers("text/plain"; extra=[
+                                                                            "Access-Control-Allow-Headers"=>"Authorization, *",
+                                                                            "Access-Control-Allow-Methods"=>"GET, PUT, DELETE",
+                                                                           ]), "ok")
 
         elseif req.method == "GET"
             if req.target == "/"
-                return HTTP.Response(200, response_headers("text/plain"), "Welcome to Primal Blossom server.")
+                return HTTP.Response(200, response_headers("text/plain"), "Welcome to Primal Blossom server. Implemented: BUD-01, BUD-02, BUD-04, BUD-05")
 
             elseif startswith(req.target, "/list/")
                 pk = Nostr.PubKeyId(string(split(req.target, '/')[end]))
@@ -151,7 +185,7 @@ function blossom_handler(req::HTTP.Request)
                 for (mimetype, created_at, path, size, sha256) in pex("select mimetype, created_at, path, size, sha256 from media_uploads where pubkey = ?1", [pk])[2]
                     # _, ext = splitext(path)
                     # mimetype = mimetype_for_ext(ext)
-                    ext = get(Main.Media.mimetype_ext, mimetype, "application/octet-stream")
+                    ext = get(Main.Media.mimetype_ext, mimetype, "")
                     push!(res, (;
                                 url="$(BASE_URL[])/$(bytes2hex(sha256))$(ext)",
                                 sha256=bytes2hex(sha256),
@@ -167,21 +201,29 @@ function blossom_handler(req::HTTP.Request)
                 if !isnothing(r)
                     return HTTP.Response(302, response_headers(r.mimetype; extra=["Location"=>"https://primal.b-cdn.net/media-cache?s=o&a=1&u=$(URIs.escapeuri(r.media_url))"]), "redirecting")
                 else
-                    return HTTP.Response(404, response_headers("text/plain"), "not found")
+                    blossom_error(404, "not found")
                 end
             end
 
         elseif req.method == "HEAD"
-            r = find_blob(req.target)
-            if !isnothing(r)
-                return HTTP.Response(200, response_headers(r.mimetype), "")
+            if req.target in ["/upload", "/media"]
+                h = 
+                try hex2bytes(get_header(req.headers, "X-SHA-256"))
+                catch _ blossom_error(401, "X-SHA-256 request header is missing") end
+                check_action(req, "upload"; x_tag_hash=h)
+                return HTTP.Response(200, response_headers("text/plain"), "ok")
             else
-                return HTTP.Response(404, response_headers("text/plain"), "not found")
+                r = find_blob(req.target)
+                if !isnothing(r)
+                    return HTTP.Response(200, response_headers(r.mimetype), "")
+                else
+                    blossom_error(404, "not found")
+                end
             end
 
         elseif req.method == "DELETE"
-            e = check_action(req, "delete")
             r = find_upload(req.target)
+            e = check_action(req, "delete"; x_tag_hash=r.sha256)
             if !isnothing(r)
                 pex("delete from media_uploads where pubkey = ?1 and sha256 = ?2", [e.pubkey, r.sha256])
                 if isempty(pex("select 1 from media_uploads where sha256 = ?1 limit 1", [r.sha256])[2])
@@ -200,35 +242,50 @@ function blossom_handler(req::HTTP.Request)
                 end
                 return HTTP.Response(200, response_headers("text/plain"), "ok")
             else
-                return HTTP.Response(404, response_headers("text/plain"), "not found")
+                blossom_error(404, "not found")
             end
 
         elseif req.method == "PUT"
-            e = check_action(req, "upload")
             # push!(Main.stuff, (:blossomupload, req))
-            data = collect(req.body)
-            r = JSON.parse([x for x in Main.App.import_upload_2(est[], e.pubkey, data; strip_metadata=false) 
-                                  if x.kind == Int(Main.App.UPLOADED_2)][1].content)
-            sha256 = hex2bytes(r["sha256"])
-            for (mimetype, created_at, path, size) in pex("select mimetype, created_at, path, size from media_uploads where sha256 = ?1", [sha256])[2]
-                # @show path
-                # _, ext = splitext(path)
-                # mimetype = mimetype_for_ext(ext)
-                ext = get(Main.Media.mimetype_ext, mimetype, "application/octet-stream")
-                return HTTP.Response(200, response_headers("application/json"), 
-                                     JSON.json((;
-                                                url="$(BASE_URL[])/$(bytes2hex(sha256))$(ext)",
-                                                sha256=bytes2hex(sha256),
-                                                size,
-                                                type=mimetype,
-                                                created=created_at,
-                                               )))
+            if req.target == "/mirror"
+                url = JSON.parse(String(req.body))["url"]
+                h = hex2bytes(match(r"/([0-9a-fA-F]{64})", url)[1])
+                e = check_action(req, "upload"; x_tag_hash=h)
+                data = Media.download(est[], url; timeout=60)
+                return import_blob(e, data; strip_metadata=false)
+            else
+                data = collect(req.body)
+                e = check_action(req, "upload"; x_tag_hash=SHA.sha256(data))
+                strip_metadata = req.target == "/media"
+                return import_blob(e, data; strip_metadata)
             end
-            return HTTP.Response(404, response_headers("text/plain"), "not found")
         end
 
-        return HTTP.Response(404, response_headers("text/plain"), "not found")
+        blossom_error(404, "not found")
     end
+end
+
+function import_blob(e::Nostr.Event, data::Vector{UInt8}; strip_metadata::Bool)
+    h = SHA.sha256(data)
+    r = JSON.parse([x for x in Main.App.import_upload_2(est[], e.pubkey, data; strip_metadata) 
+                    if x.kind == Int(Main.App.UPLOADED_2)][1].content)
+    sha256 = hex2bytes(r["sha256"])
+    strip_metadata || @assert h == sha256
+    for (mimetype, created_at, path, size) in pex("select mimetype, created_at, path, size from media_uploads where sha256 = ?1", [sha256])[2]
+        # @show path
+        # _, ext = splitext(path)
+        # mimetype = mimetype_for_ext(ext)
+        ext = get(Main.Media.mimetype_ext, mimetype, "")
+        return HTTP.Response(200, response_headers("application/json"), 
+                             JSON.json((;
+                                        url="$(BASE_URL[])/$(bytes2hex(sha256))$(ext)",
+                                        sha256=bytes2hex(sha256),
+                                        size,
+                                        type=mimetype,
+                                        created=created_at,
+                                       )))
+    end
+    blossom_error(404, "not found")
 end
 
 end
